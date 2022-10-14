2baa0ffe67af69ab6b03fb7bb722aaab97845013
brytchukz <brightchukwu2015@gmail.com>
2022-10-14 14:41:52 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# Installs a package
+# Install a package using Puppet
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
+
 package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
 }
